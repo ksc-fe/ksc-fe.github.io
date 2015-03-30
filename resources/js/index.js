d99309ba716a55440da8dc02920e407742aafbf1
kreding <juxuejian1@163.com>
2015-03-30 18:17:57 +0800

    init

---
+++ resources/js/index.js
@@ -0,0 +1,17 @@
+$(function() {
+    init();
+    bindEvent();
+})
+
+function init() {
+// background-image
+   $(document).ready(function(){
+        $("body").css("background-image","url(resources/images/temp/9.png)");
+    });   
+}
+
+function bindEvent() {
+
+
+}
+
